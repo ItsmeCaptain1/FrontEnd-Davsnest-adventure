2d815c804bbb18729e9bbf13fdeb16deb5b67c8d
Bhuresh Kumar <bhureshsoni7@gmail.com>
2021-06-14 08:39:25 +0530

    Day5 THA calculator

---
+++ Day5/class.js
@@ -0,0 +1,50 @@
+// JS is lexically scoped language
+
+// const arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i], i);
+// }
+
+// let sum = 0;
+// for (let item of arr) {
+//   sum += item;
+// }
+// console.log("sum = ", sum);
+
+// arr.forEach((item) => {
+//   console.log(item);
+// });
+
+// console.log(" ");
+
+// arr.forEach((item, index) => {
+//   console.log(item, index);
+// });
+
+// about type
+// var x = 10;
+// console.log(typeof x);
+// x = "hello";
+// console.log(typeof x);
+
+// var a = "10";
+// var b = 10;
+// console.log(a + b);
+// console.log(Number(a) + b);
+// console.log(+x + y);
+
+// var x = 0;
+// console.log(Boolean(x));
+// console.log(typeof (x + ""));
+
+// var x = 10;
+// var y = "10";
+// console.log(x == y);
+// console.log(x === y);
+
+// var x = 10;
+// function fun() {
+//   var x = 30;
+//   console.log(x);
+// }
+// fun();
