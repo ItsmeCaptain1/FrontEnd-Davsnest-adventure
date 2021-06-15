a0aecda137b4202770207ebc34ccffb3350cbbb6
Bhuresh Kumar <bhureshsoni7@gmail.com>
2021-06-15 09:29:06 +0530

    Day6 class:

---
+++ Day6/class.js
@@ -0,0 +1,68 @@
+a1 = [1, 2, 3, 3, 4, 5];
+b1 = [6, 7, 7, 8, 9];
+c1 = [1, 1, 1, 1];
+
+// Accessing Elements
+
+// console.log(a1[2]);
+// console.log(a1.concat(b1));
+// console.log(a1.length);
+// console.log(a1.join("--"));
+// console.log(a1.slice(1, 3));
+
+// console.log(a1.indexOf(3));
+// console.log(a1.lastIndexOf(3));
+
+// a1.forEach((item, index) => {
+//   console.log(item, index);
+// });
+
+// for (let item of b1) {
+//   console.log(item);
+// }
+
+// for (let index in b1) {
+//   console.log(index, b1[index]);
+// }
+
+// for (let i = 0; i < c1.length; i++) {
+//   console.log(i, c1[i]);
+// }
+
+// console.log(a1.some((item) => item === 4));
+// console.log(c1.every((item) => item === 1));
+// console.log(b1.filter((item) => item === 7));
+
+// console.log(a1.map((item) => console.log(item)));
+// console.log(a1.map((item) => (item = 20)));
+
+// const reduceFn = a1.reduce((acc, item) => {
+//   acc = acc + item;
+//   return acc;
+// }, 0);
+// console.log(reduceFn);
+
+a2 = [5, 6, 71, 8, 9];
+// console.log(a2.pop());
+// console.log(a2);
+
+// console.log(a2.length);
+// console.log(a2.push(1000));
+
+// b2 = a2.slice(0) ; // copy a2 into b2
+// console.log(a2.reverse());
+
+// list1 = ["b", "c", "a"];
+// console.log(list1.sort());
+
+// list2 = [2, 3, 1];
+// console.log(list2.sort((a, b) => b - a));
+
+// console.log(a1.splice(1, 1, 5));
+
+// console.log(a1.unshift(40)); // push_front
+// console.log(a1.shift()); // pop_front
+
+e1 = "this is string";
+console.log(typeof e1);
+console.log(Array.isArray(a2));
