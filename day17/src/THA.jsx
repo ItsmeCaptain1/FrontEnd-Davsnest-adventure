af6fbe08a10128bc5e65e0c31b2a7eba4a147ef4
Bhuresh Kumar <bhureshsoni7@gmail.com>
2021-07-16 20:34:49 +0530

    update17

--- day17/src/THA.jsx
+++ day17/src/THA.jsx
@@ -2,14 +2,13 @@ import THA1 from "./THA1";
 const Card = (props) => {
   return (
     <div className="cont">
-      <THA1 />
-      <THA1 />
-      <THA1 />
-      <THA1 />
-      <THA1 />
-      <THA1 />
-      <THA1 />
-      <THA1 />
+      <THA1 name="Apple" price="60" />
+      <THA1 name="Banana" price="20" />
+      <THA1 name="Orange" price="50" />
+      <THA1 name="Water Melon" price="45" />
+      <THA1 name="Pineapple" price="90" />
+      <THA1 name="Grapes" price="55" />
+      <THA1 name="Date" price="35" />
     </div>
   );
 };
--- day17/src/THA1.jsx
+++ day17/src/THA1.jsx
@@ -1,8 +1,8 @@
 const THA1 = (props) => {
   return (
     <div className="card1">
-      <h1 className="item">Item</h1>
-      <h4 className="detail">Detail</h4>
+      <h1 className="item">{props.name}</h1>
+      <h4 className="detail">Price of fruit is {props.price} PrKg</h4>
     </div>
   );
 };
