4707d8b063f9ba2f716e7d1962a4826d9d803121
Bhuresh Kumar <bhureshsoni7@gmail.com>
2021-07-25 16:19:11 +0530

    Day21 finished

--- Day21/src/App.jsx
+++ Day21/src/App.jsx
@@ -3,38 +3,51 @@ import "./App.css";
 
 function App() {
   const [items, setItems] = useState([
-    { itemName: "Apple", calorieAmount: 55 },
-    { itemName: "Watermalon", calorieAmount: 35 },
-    { itemName: "Banana", calorieAmount: 45 },
+    { itemName: "Apple", calorieAmount: 55, isEditing: false },
+    { itemName: "Watermalon", calorieAmount: 35, isEditing: false },
+    { itemName: "Banana", calorieAmount: 45, isEditing: false },
   ]);
 
   const [inputValue, setInputValue] = useState({
     itemName: "",
     calorieAmount: "",
+    isEditing: false,
   });
 
   const ADD_NEW_ITEM = () => {
     const newItem = {
       itemName: inputValue.itemName,
       calorieAmount: inputValue.calorieAmount,
+      isEditing: false,
     };
     const newItems = [...items, newItem];
     setItems(newItems);
-    setInputValue({ itemName: "", calorieAmount: "" });
+    setInputValue({ itemName: "", calorieAmount: "", isEditing: false });
   };
 
   const deleteItem = (index) => {
     const newItems = items.filter((item, i) => i !== index);
-
     setItems(newItems);
   };
 
-  const editItem = (index) => {};
+  const editItem = (index) => {
+    const newList = [...items];
+    newList[index].isEditing = newList[index].isEditing ? false : true;
+    setItems(newList);
+  };
+
+  const updateItem = (index, newItem) => {
+    setItems(
+      items.map((item, i) => (i === index ? { ...item, ...newItem } : item))
+    );
+  };
 
   return (
     <div className="main-container">
       <div className="add-item-box">
         <input
+          type="text"
+          required
           value={inputValue.itemName}
           onChange={(e) =>
             setInputValue({ ...inputValue, [e.target.name]: e.target.value })
@@ -43,6 +56,8 @@ function App() {
           placeholder="Add an item"
         />
         <input
+          type="number"
+          required
           value={inputValue.calorieAmount}
           onChange={(e) =>
             setInputValue({ ...inputValue, [e.target.name]: e.target.value })
@@ -55,13 +70,38 @@ function App() {
       <div className="item-list">
         {items.map((item, index) => (
           <div className="item-container">
-            <h3>{item.itemName}</h3>
-            <div>You have consumed {item.calorieAmount} calories</div>
+            {item.isEditing ? (
+              <>
+                <input
+                  type="text"
+                  name="itemName"
+                  required
+                  value={item.itemName}
+                  onChange={(e) =>
+                    updateItem(index, { itemName: e.target.value })
+                  }
+                />
+                <input
+                  type="number"
+                  name="calorieAmount"
+                  required
+                  value={item.calorieAmount}
+                  onChange={(e) =>
+                    updateItem(index, { calorieAmount: e.target.value })
+                  }
+                />
+              </>
+            ) : (
+              <>
+                <h3>{item.itemName}</h3>
+                <div>You have consumed {item.calorieAmount} calories</div>
+              </>
+            )}
             <button type="button" onClick={() => deleteItem(index)}>
               Delete
             </button>
             <button type="button" onClick={() => editItem(index)}>
-              Edit
+              {item.isEditing ? "Done" : "Edit"}
             </button>
           </div>
         ))}
